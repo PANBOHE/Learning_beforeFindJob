4cb7512cb2e6abdea62f773968af48012bb42355
PANBOHE <panbohero@outlook.com>
2018-04-04 15:05:36 +0800

    Create learn_0404.h
    希尔的这个for循环太可怕了。不稳定
    比插入排序又要好，尤其是数组相对稳定时候

---
+++ learn_0404.h
@@ -0,0 +1,34 @@
+#include "iostream"
+#include "stdio.h"
+using namespace std;
+
+//0404 希尔排序
+void swapInt(int *a,int *b)
+{
+	int c=*a;
+    *a=*b;
+	*b=c;
+	
+}
+
+void shell(int *data,int len)
+{
+	if(len<=1 || data==NULL)
+		return;
+	for(int div=len/2;div>=1;div=div/2)
+	{
+		for(int i=0;i<div;++i)
+		{
+			for(int j=i;j<len-div;j=j+div)
+			{
+				for(int k=j;k<len;k=k+div)
+				{
+					if(data[j]>data[k])
+					{
+						swapInt(data+j,data+k);
+					}
+				}
+			}
+		}
+	}
+}
